f778ce902080a6c78738561d129d8e4dcec64f93
Dan Cassidy <dan.cassidy.1983gmail.com>
2016-10-02 15:04:55 -0400

    Current changes.

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,156 +1,184 @@
 #include <cstddef>
+#include <cstdio>
 #include <chrono>
 #include <fstream>
-#include <iostream>
+#include <omp.h>
 #include <string>
-#include "image_kernels.h"
 #include "utility.h"
 using namespace std;
 
+const uint32_t KERNEL_FILL_VALUE = 1;
 const string SIGNATURE_PGM_BINARY = "P5";
-
-// A small selection of kernels to use.
-// const image_kernels::image_kernel_3 & KERNEL = image_kernels::BOX_BLUR_3; const uint32_t KERNEL_SIZE = image_kernels::KERNEL_SIZE_3;
-// const image_kernels::image_kernel_5 & KERNEL = image_kernels::BOX_BLUR_5; const uint32_t KERNEL_SIZE = image_kernels::KERNEL_SIZE_5;
-const image_kernels::image_kernel_91 & KERNEL = image_kernels::BOX_BLUR_91; const uint32_t KERNEL_SIZE = image_kernels::KERNEL_SIZE_91;
-
-// Declare the offset to be used with the kernel.
-const uint32_t KERNEL_OFFSET = KERNEL_SIZE / 2;
 
 int main(int32_t argc, const char * argv[])
 {
     uint8_t ** input_data = nullptr, ** output_data = nullptr;
+    uint32_t ** kernel = nullptr;
 
-    uint32_t height = 0, width = 0, depth = 0;
-    uint32_t to_return = 0;
+    int32_t height = 0, width = 0, depth = 0;
+    int32_t kernel_size = 0, kernel_offset = 0;
+
     uint32_t number_of_threads = 0;
+
     string input_filename, output_filename, signature;
     fstream input_stream, output_stream;
+
+    uint32_t to_return = 0;
 
     chrono::steady_clock::time_point timer_begin, timer_end;
     chrono::duration<double> timer_duration;
 
+
     // Determine the action to take based upon the number of parameters.
     switch (argc)
     {
-        case 4:
-            // Read and process, then write the result back to disk.
-            output_filename = argv[3];
+        // Read and process, then write the result back to disk.
+        case 5:
+            output_filename = argv[4];
+            // Utilize fall-through to read the rest of the command line arguments.
 
-        case 3:
-            // Read and process, but don't write.
-            input_filename = argv[2];
+        // Read and process, but don't write.
+        case 4:
+            input_filename = argv[3];
+            kernel_size = stoi(argv[2]);
             number_of_threads = stoi(argv[1]);
+            kernel_offset = kernel_size / 2;
             break;
 
+        // No arguments, or more than the max number of arguments.  Spit out the syntax.
         default:
-            // No arguments, or more than two arguments.  Spit out the syntax.
-            cerr << "Syntax: parallel-convolution.exe <num_threads> <input_filename> [output_filename]\n";
+            fprintf(stderr, "Syntax: parallel-convolution.exe <num_threads> <kernel_size> <input_filename> [output_filename]\n");
             to_return = 1;
-            goto exit_program; // That's right, I'm using goto. Why? Because I can, really. It's good to switch things up occasionally.
+            goto exit_program; // Using goto to jump to a central resource deallocation area.
     }
 
-    cout << "Number of threads: " << number_of_threads << "\n";
-    cout << "Kernel size: " << KERNEL_SIZE << "x" << KERNEL_SIZE << "\n";
+
+    // Create and fill the kernel.
+    kernel = utility::create_array_2d<uint32_t>(kernel_size, kernel_size);
+    utility::fill_array_2d(kernel, kernel_size, kernel_size, KERNEL_FILL_VALUE);
+    printf("Kernel size: %dx%d\n", kernel_size, kernel_size);
+
 
     // Attempt to open the specified file for binary input.
     input_stream.open(input_filename, ios_base::in | ios_base::binary);
     if (input_stream.fail())
     {
-        cerr << "Something went wrong when opening the requested input file.\n";
+        fprintf(stderr, "Something went wrong when opening the requested input file.\n");
         to_return = 1;
         goto exit_program;
     }
+
 
     // Get the first line of the PGM, its "magic number."
     getline(input_stream, signature);
     if (signature != SIGNATURE_PGM_BINARY)
     {
-        cerr << "The input file is not a binary PGM.\n";
+        fprintf(stderr, "The input file is not a binary PGM.\n");
         to_return = 1;
         goto exit_program;
     }
+
 
     // Read the width, height, and depth from the input file.
     input_stream >> width >> height >> depth;
 
+
     // Eat the next character, which should be '\n'.
     input_stream.get();
     
+
     // Check the width, height, and depth to make sure they are valid.
-    if (width == 0 || height == 0 || depth == 0)
+    if (width <= 0 || height <= 0 || depth <= 0)
     {
-        cerr << "Invalid dimensions or depth in input file.\n";
+        fprintf(stderr, "Invalid dimensions or depth in input file.\n");
         to_return = 1;
         goto exit_program;
     }
-    cout << "Image dimensions: " << width << "x" << height << "\n";
-
+    printf("Image dimensions: %dx%d", width, height);
+    
     // Create the array to read the file into.
-    input_data = utility::create_array_2d<uint8_t>(height + KERNEL_SIZE - 1, width + KERNEL_SIZE - 1);
+    input_data = utility::create_array_2d<uint8_t>(height + kernel_size - 1, width + kernel_size - 1);
+
 
     // Read the input file, then close the input stream.
-    for (uint32_t row = KERNEL_OFFSET; row < height + KERNEL_OFFSET; row++)
-        input_stream.read((char *)&(input_data[row][KERNEL_OFFSET]), width);
+    for (int32_t row = kernel_offset; row < height + kernel_offset; row++)
+        input_stream.read((char *)&(input_data[row][kernel_offset]), width);
     input_stream.close();
 
+
     // Extend the input array.
-    timer_begin = chrono::steady_clock::now();
-    utility::extend_array(input_data, height, width, KERNEL_SIZE);
-    timer_end = chrono::steady_clock::now();
-    timer_duration = chrono::duration_cast<chrono::duration<double>>(timer_end - timer_begin);
-    cout << "Array extension took " << timer_duration.count() << " second(s).\n";
+    utility::extend_array(input_data, height, width, kernel_size);
+
 
     // Create the array to write the modified data into.
     output_data = utility::create_array_2d<uint8_t>(height, width);
 
+
     // Do the convolutions, and time it.
+    printf("Number of threads requested: %d\n", number_of_threads);
     timer_begin = chrono::steady_clock::now();
-    #pragma omp parallel for num_threads(number_of_threads) default(none) \
-        private(data_row, data_column, kernel_row, kernel_column, accumulator) \
-        shared(output_data)
-    for (uint32_t data_row = KERNEL_OFFSET; data_row < height + KERNEL_OFFSET; data_row++)
+    #pragma omp parallel num_threads(number_of_threads) default(none) \
+        shared(height, width, input_data, output_data, kernel, kernel_size, kernel_offset)
     {
-        for (uint32_t data_column = KERNEL_OFFSET; data_column < width + KERNEL_OFFSET; data_column++)
+        #pragma omp master
+        printf("Number of threads created: %d\n", omp_get_num_threads());
+        
+        #pragma omp for
+        // Iterate through the full input data.
+        for (int32_t data_row = kernel_offset; data_row < height + kernel_offset; data_row++)
         {
-            uint32_t accumulator = 0;
-
-            for (uint32_t kernel_row = 0; kernel_row < KERNEL_SIZE; kernel_row++)
+            for (int32_t data_column = kernel_offset; data_column < width + kernel_offset; data_column++)
             {
-                for (uint32_t kernel_column = 0; kernel_column < KERNEL_SIZE; kernel_column++)
-                {
-                    accumulator += input_data[data_row + kernel_row - KERNEL_OFFSET]
-                                             [data_column + kernel_column - KERNEL_OFFSET] * 
-                                   KERNEL[kernel_row][kernel_column];
-                }
-            }
+                int32_t sum = 0;
 
-            output_data[data_row - KERNEL_OFFSET][data_column - KERNEL_OFFSET] = accumulator / (KERNEL_SIZE * KERNEL_SIZE);
+                // Iterate through the full kernel.
+                for (int32_t kernel_row = 0; kernel_row < kernel_size; kernel_row++)
+                {
+                    for (int32_t kernel_column = 0; kernel_column < kernel_size; kernel_column++)
+                    {
+                        // Multiply each data element by its corresponding kernel element and add it
+                        // to a running sum.
+                        sum += input_data[data_row + kernel_row - kernel_offset]
+                            [data_column + kernel_column - kernel_offset] *
+                            kernel[kernel_row][kernel_column];
+                    }
+                }
+
+                // Once the summation is done, store that value in the output array.
+                output_data[data_row - kernel_offset][data_column - kernel_offset] = sum / (kernel_size * kernel_size);
+            }
         }
     }
     timer_end = chrono::steady_clock::now();
+    
 
     // Calculate and display the time taken specifically for the convolutions.
     timer_duration = chrono::duration_cast<chrono::duration<double>>(timer_end - timer_begin);
-    cout << "Convolutions took " << timer_duration.count() << " second(s).\n\n";
+    printf("Convolutions took %f second(s).\n\n", timer_duration.count());
+
 
     // If an output file is specified, attempt to open it for binary output.
     if (output_filename.empty() == false)
     {
+        // Open the file.
         output_stream.open(output_filename, ios_base::out | ios_base::binary);
+        // Verify that the file was opened.
         if (output_stream.fail())
         {
-            cerr << "Something went wrong when opening the requested output file.\n";
+            fprintf(stderr, "Something went wrong when opening the requested output file.\n");
             to_return = 1;
             goto exit_program;
         }
 
+        // Write the PGM header.
         output_stream << signature << "\n";
         output_stream << width << " " << height << "\n";
         output_stream << depth << "\n";
-        for (uint32_t row = 0; row < height; row++)
+        // Write the PGM data.
+        for (int32_t row = 0; row < height; row++)
             output_stream.write((char *)(output_data[row]), width);
     }
+
 
     // Clean up the file streams and arrays then exit.
 exit_program:
@@ -158,6 +186,8 @@ exit_program:
         input_stream.close();
     if (output_stream.is_open())
         output_stream.close();
+    if (kernel != nullptr)
+        utility::delete_array_2d(kernel);
     if (input_data != nullptr)
         utility::delete_array_2d(input_data);
     if (output_data != nullptr)
--- HW2/utility.h
+++ HW2/utility.h
@@ -3,6 +3,7 @@
 #define UTILITY_H
 
 #include <cstdint>
+#include <cstring>
 using namespace std;
 
 namespace utility
@@ -10,90 +11,45 @@ namespace utility
 
 // Extend an array for kernel convolution.  Assumes that the kernel is an odd height and width.
 template <typename T>
-void extend_array(T ** array_to_extend, const uint32_t height, const uint32_t width,
-                           const uint32_t kernel_size)
+void extend_array(T ** array_to_extend, const int32_t height, const int32_t width, const int32_t kernel_size)
 {
-    const uint32_t KERNEL_OFFSET = kernel_size / 2;
+    int32_t kernel_offset = kernel_size / 2;
 
     // Extend existing data left.
-    for (uint32_t row = KERNEL_OFFSET; row < height + KERNEL_OFFSET - 1; row++)
+    for (int32_t row = kernel_offset; row < height + kernel_offset - 1; row++)
     {
-        for (uint32_t column = KERNEL_OFFSET; column > 0; column--)
+        for (int32_t column = kernel_offset; column > 0; column--)
         {
             array_to_extend[row][column - 1] = array_to_extend[row][column];
         }
     }
 
     // Extend existing data right.
-    for (uint32_t row = KERNEL_OFFSET; row < height + KERNEL_OFFSET - 1; row++)
+    for (int32_t row = kernel_offset; row < height + kernel_offset - 1; row++)
     {
-        for (uint32_t column = width + KERNEL_OFFSET - 1; column < width + kernel_size - 2; column++)
+        for (int32_t column = width + kernel_offset - 1; column < width + kernel_size - 2; column++)
         {
             array_to_extend[row][column + 1] = array_to_extend[row][column];
         }
     }
 
     // Extend all data upwards.
-    for (uint32_t row = KERNEL_OFFSET; row > 0; row--)
+    for (int32_t row = kernel_offset; row > 0; row--)
     {
-        for (uint32_t column = 0; column < width + kernel_size - 1; column++)
-        {
-            array_to_extend[row - 1][column] = array_to_extend[row][column];
-        }
+        memcpy(array_to_extend[row], array_to_extend[row - 1], (width + kernel_size - 1) * sizeof(T));
     }
 
     // Extend all data downwards.
-    for (uint32_t row = height + KERNEL_OFFSET - 1; row < height + kernel_size - 2; row++)
+    for (int32_t row = height + kernel_offset - 1; row < height + kernel_size - 2; row++)
     {
-        for (uint32_t column = 0; column < width + kernel_size - 1; column++)
-        {
-            array_to_extend[row + 1][column] = array_to_extend[row][column];
-        }
+        memcpy(array_to_extend[row], array_to_extend[row + 1], (width + kernel_size - 1) * sizeof(T));
     }
-
-    //// Extend existing data upwards.
-    //for (uint32_t row = KERNEL_OFFSET; row > 0; row--)
-    //{
-    //    for (uint32_t column = KERNEL_OFFSET; column < width + KERNEL_OFFSET; column++)
-    //    {
-    //        array_to_extend[row - 1][column] = array_to_extend[row][column];
-    //    }
-    //}
-
-    //// Extend existing data downwards.
-    //for (uint32_t row = height + KERNEL_OFFSET - 1; row < height + kernel_size - 2; row++)
-    //{
-    //    for (uint32_t column = KERNEL_OFFSET; column < width + KERNEL_OFFSET; column++)
-    //    {
-    //        array_to_extend[row + 1][column] = array_to_extend[row][column];
-    //    }
-    //}
-
-    //// Extend all data left.
-    //for (uint32_t row = 0; row < height + kernel_size - 1; row++)
-    //{
-    //    for (uint32_t column = KERNEL_OFFSET; column > 0; column--)
-    //    {
-    //        array_to_extend[row][column - 1] = array_to_extend[row][column];
-    //    }
-    //}
-
-    //// Extend all data right.
-    //for (uint32_t row = 0; row < height + kernel_size - 1; row++)
-    //{
-    //    for (uint32_t column = width + KERNEL_OFFSET - 1; column < width + kernel_size - 2; column++)
-    //    {
-    //        array_to_extend[row][column + 1] = array_to_extend[row][column];
-    //    }
-    //}
-
-    return;
 }
 
 // Taken from http://stackoverflow.com/a/21944048
 // Create a 2 dimensional array of contiguous data.
 template <typename T>
-T ** create_array_2d(uint32_t height, uint32_t width)
+T ** create_array_2d(int32_t height, int32_t width)
 {
     // Allocate pointers.
     T ** ptr = new T*[height];
@@ -102,7 +58,7 @@ T ** create_array_2d(uint32_t height, uint32_t width)
     T * pool = new T[height * width];
 
     // Assign each "row" in the pool to a pointer.
-    for (uint32_t i = 0; i < height; ++i, pool += width)
+    for (int32_t i = 0; i < height; ++i, pool += width)
     {
         ptr[i] = pool;
     }
@@ -116,6 +72,19 @@ void delete_array_2d(T ** arr)
 {
     delete[] arr[0];  // remove the pool
     delete[] arr;     // remove the pointers
+}
+
+// Fill a 2 dimensional array.  For safety, this should really only be used with simple data types.
+template <typename T>
+void fill_array_2d(T ** arr, int32_t height, int32_t width, T fill_data)
+{
+    for (int32_t row = 0; row < height; row++)
+    {
+        for (int32_t column = 0; column < width; column++)
+        {
+            arr[row][column] = fill_data;
+        }
+    }
 }
 
 } // end namespace utility
