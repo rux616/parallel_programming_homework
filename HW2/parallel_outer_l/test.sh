177a26c53b461d10788791160a570712a5023c28
Dan Cassidy <dan.cassidy.1983gmail.com>
2016-10-02 15:04:43 -0400

    A small script for automated testing in the Windows Linux subsystem.

---
+++ HW2/parallel_outer_l/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for((i=$1;i<=$2;i+=1))
+do
+	echo 'Number of threads: ' $i
+	for((j=1;j<=$3;j+=1))
+	do
+		echo 'Test iteration ' $j ' of ' $3
+		./parallel_convolutions $i $4 ../data/input.pgm >> output.log
+	done
+done
