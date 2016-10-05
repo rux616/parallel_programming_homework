ded0346b20f43aebeaa6107ad6800914c2461dfb
Dan Cassidy <dan.cassidy.1983gmail.com>
2016-10-04 21:30:15 -0400

    Renamed variables to be more descriptive and added function headers.

--- HW2/utility.h
+++ HW2/utility.h
@@ -1,3 +1,13 @@
+/*--------------------------------------------------------------------------------------------------
+ * Author:      Dan Cassidy
+ * Date:        2016-10-04
+ * Assignment:  Homework 2
+ * Source File: utility.h
+ * Language:    C/C++
+ * Course:      CSCI-B 424 Parallel Programming
+ * Purpose:     Contains some utility template functions to make my life easier.
+ -------------------------------------------------------------------------------------------------*/
+
 #pragma once
 #ifndef UTILITY_H
 #define UTILITY_H
@@ -9,9 +19,19 @@ using namespace std;
 namespace utility
 {
 
-// Extend an array for kernel convolution.  Assumes that the kernel is an odd height and width.
+/*--------------------------------------------------------------------------------------------------
+ * Name:    extend_array
+ * Purpose: Copies the edge data of an sub 2d array out to the edges of the containing 2d array.
+ * Input:   T ** array_to_extend, holds the pointer to the array which will be worked on.
+ * Input:   const int32_t height, holds the height of the sub array.
+ * Input:   const int32_t width, holds the width of the sub array.
+ * Input:   const int32_t kernel_size, holds the size of the kernel that will be ultimately be used
+ *          on the image.
+ * Output:  T ** array_to_extend.
+--------------------------------------------------------------------------------------------------*/
 template <typename T>
-void extend_array(T ** array_to_extend, const int32_t height, const int32_t width, const int32_t kernel_size)
+void extend_array(T ** array_to_extend, const int32_t height, const int32_t width,
+                  const int32_t kernel_size)
 {
     int32_t kernel_offset = kernel_size / 2;
 
@@ -36,23 +56,34 @@ void extend_array(T ** array_to_extend, const int32_t height, const int32_t widt
     // Extend all data upwards.
     for (int32_t row = kernel_offset; row > 0; row--)
     {
-        memcpy(array_to_extend[row], array_to_extend[row - 1], (width + kernel_size - 1) * sizeof(T));
+        memcpy(array_to_extend[row], array_to_extend[row - 1], (width + kernel_size - 1) *
+               sizeof(T));
     }
 
     // Extend all data downwards.
     for (int32_t row = height + kernel_offset - 1; row < height + kernel_size - 2; row++)
     {
-        memcpy(array_to_extend[row], array_to_extend[row + 1], (width + kernel_size - 1) * sizeof(T));
+        memcpy(array_to_extend[row], array_to_extend[row + 1], (width + kernel_size - 1) *
+               sizeof(T));
     }
 }
 
-// Taken from http://stackoverflow.com/a/21944048
-// Create a 2 dimensional array of contiguous data.
+
+
+/*--------------------------------------------------------------------------------------------------
+ * Name:    create_array_2d
+ * Purpose: Creates a 2 dimensional array of contiguous data.
+ * Input:   int32_t height, holds the number of rows the array should have.
+ * Input:   int32_t width, holds the number of columns the array should have.
+ * Output:  T **, holds a pointer to the newly created 2 dimensional array.
+ *
+ * Note:    Taken from http://stackoverflow.com/a/21944048.
+--------------------------------------------------------------------------------------------------*/
 template <typename T>
 T ** create_array_2d(int32_t height, int32_t width)
 {
     // Allocate pointers.
-    T ** ptr = new T*[height];
+    T ** new_array_pointer = new T*[height];
 
     // Allocate pool.
     T * pool = new T[height * width];
@@ -60,29 +91,50 @@ T ** create_array_2d(int32_t height, int32_t width)
     // Assign each "row" in the pool to a pointer.
     for (int32_t i = 0; i < height; ++i, pool += width)
     {
-        ptr[i] = pool;
+        new_array_pointer[i] = pool;
     }
 
-    return ptr;
+    return new_array_pointer;
 }
 
-// Delete a 2 dimensional array.
+
+
+/*--------------------------------------------------------------------------------------------------
+ * Name:    delete_array_2d
+ * Purpose: Deletes a 2 dimensional array.
+ * Input:   T ** array_to_delete, holds the pointer to the 2 dimensional array that shall be
+ *          deleted.
+ * Output:  Nothing.
+ *
+ * Note:    Taken from http://stackoverflow.com/a/21944048.
+--------------------------------------------------------------------------------------------------*/
 template <typename T>
-void delete_array_2d(T ** arr)
+void delete_array_2d(T ** array_to_delete)
 {
-    delete[] arr[0];  // remove the pool
-    delete[] arr;     // remove the pointers
+    delete[] array_to_delete[0];  // remove the pool
+    delete[] array_to_delete;     // remove the pointers
 }
 
-// Fill a 2 dimensional array.  For safety, this should really only be used with simple data types.
+
+
+/*--------------------------------------------------------------------------------------------------
+ * Name:    fill_array_2d
+ * Purpose: Fills a 2 dimensional array.  For safety, this should really only be used with primitive
+ *          data types.
+ * Input:   T ** array_to_fill, holds the pointer to the array which will be filled.
+ * Input:   int32_t height, holds the height of the array.
+ * Input:   int32_t width, holds the width of the array.
+ * Input:   T fill_data, holds the data with which the array shall be filled.
+ * Output:  T ** array_to_fill.
+--------------------------------------------------------------------------------------------------*/
 template <typename T>
-void fill_array_2d(T ** arr, int32_t height, int32_t width, T fill_data)
+void fill_array_2d(T ** array_to_fill, int32_t height, int32_t width, T fill_data)
 {
     for (int32_t row = 0; row < height; row++)
     {
         for (int32_t column = 0; column < width; column++)
         {
-            arr[row][column] = fill_data;
+            array_to_fill[row][column] = fill_data;
         }
     }
 }
